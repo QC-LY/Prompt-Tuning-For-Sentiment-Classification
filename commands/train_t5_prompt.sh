8a3c24a7e88419980291ce0b78f3fe583363f3c6
Yuanhuiyi Lv <56156806+QC-LY@users.noreply.github.com>
2023-04-05 15:21:32 +0800

    Update train_t5_prompt.sh

--- commands/train_t5_prompt.sh
+++ commands/train_t5_prompt.sh
@@ -1,7 +1,7 @@
 cd ../
-python prompt_train.py --train_data_path ./data/mydata/new_train.json \
---train_label_path ./data/mydata/new_train_labels.json \
---dev_data_path ./data/mydata/dev.json \
---dev_label_path ./data/mydata/dev_labels.json \
+python prompt_train.py --train_data_path ./data/train.json \
+--train_label_path ./data/train_labels.json \
+--dev_data_path ./data/dev.json \
+--dev_label_path ./data/dev_labels.json \
 --model_name_or_path t5 \
 --output_dir ./checkpoint
