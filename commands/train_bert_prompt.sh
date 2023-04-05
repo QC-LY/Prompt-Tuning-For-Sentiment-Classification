2f1a484117c5dad94754c0d987672460631c74f2
Yuanhuiyi Lv <56156806+QC-LY@users.noreply.github.com>
2023-04-05 15:22:44 +0800

    Update train_bert_prompt.sh

--- commands/train_bert_prompt.sh
+++ commands/train_bert_prompt.sh
@@ -1,6 +1,6 @@
 cd ../
-python prompt_train.py --train_data_path ./data/new_train.json \
---train_label_path ./data/new_train_labels.json \
+python prompt_train.py --train_data_path ./data/train.json \
+--train_label_path ./data/train_labels.json \
 --dev_data_path ./data/dev.json \
 --dev_label_path ./data/dev_labels.json \
 --model_name_or_path bert \
