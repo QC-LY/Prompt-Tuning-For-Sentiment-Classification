be35678df70553ff9934a0498d52ca6817d681a2
Yuanhuiyi Lv <56156806+QC-LY@users.noreply.github.com>
2023-04-05 15:18:20 +0800

    Update train.sh

--- commands/train.sh
+++ commands/train.sh
@@ -1,7 +1,7 @@
 cd ../
-python train.py --train_data_path ./data/mydata/new_train.json \
---train_label_path ./data/mydata/new_train_labels.json \
---dev_data_path ./data/mydata/dev.json \
---dev_label_path ./data/mydata/dev_labels.json \
+python train.py --train_data_path ./data/new_train.json \
+--train_label_path ./data/new_train_labels.json \
+--dev_data_path ./data/dev.json \
+--dev_label_path ./data/dev_labels.json \
 --model_name_or_path bert \
 --output_dir ./checkpoint
